59a611252f3cf963304ff11961ad5a8360acb920
Fotis Evangelou <fevangelou@users.noreply.github.com>
2019-12-21 22:07:20 +0200

    Push v1.1.0 (code cleanups)

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-Akismet For Kunena
+Akismet for Kunena
 =========
 
-**Akismet for Kunena** integrates the popular antispam service [Akismet.com](http://akismet.com/) with [Kunena 3.x](http://www.kunena.org/).
+**Akismet for Kunena** integrates the popular antispam service [Akismet.com](https://akismet.com/) with [Kunena 3.x](https://www.kunena.org/).
 
 Unlike other antispam services, Akismet does not present the visitor with an ugly 'captcha'. Instead it scans the visitor's post, name and email address and runs hundreds of tests to determine whether it's spam or not.
 
@@ -9,40 +9,22 @@ It's a completely automated way to protect your Kunena forum from spammers.
 
 [Get a free or commercial API key from akismet.com](https://akismet.com/plans/) and simply paste it in the plugin parameters. Don't forget to enable the plugin.
 
-
-### FEATURES
-Integrates the Akismet antispam service into Kunena.
-
-
-### DEMO & SHOWCASE
-A couple of Kunena based forums using this plugin for added protection against spammers:
-
-- K2 Community Forum - http://getk2.org/community
-- JoomlaWorks Forum - http://www.joomlaworks.net/forum
-
-
-### REQUIREMENTS
-This is a Kunena plugin for Joomla and the only basic requirements to use it are:
-
-- Kunena v3.x installed
-- Joomla 2.5 or 3.x installed
-- An API key from Akismet.com
-
-
-### LICENSE
-This is a Joomla extension developed by JoomlaWorks and released under the GNU/GPL v2 license.
-
-
-### ADDITIONAL INFO
-Some helpful links:
-
-- Kunena - http://www.kunena.org
-- Joomla - http://www.joomla.org
-
 If you want to provide feedback for this Joomla plugin, you may use the GitHub issue tracker here: https://github.com/joomlaworks/Akismet-For-Kunena/issues
 
 
+### DEMO
+The JoomlaWorks Forum at https://www.joomlaworks.net/forum is protected by the Akismet for Kunena plugin.
+
+
+### COMPATIBILITY
+This is a Kunena plugin for Joomla versions 2.5 & 3.x with Kunena v3.x (or newer).
+
+
+### LICENSE
+This is a Joomla extension developed by JoomlaWorks and released under the GNU/GPL license.
+
+
 ## DOWNLOAD
-You can grab the latest release here: http://goo.gl/3zTv3x (v1.0.0)
+You can grab the latest release here: http://goo.gl/3zTv3x (v1.1.0)
 
 To install this Joomla plugin, download the file in your computer and then head over to the Joomla installer and choose to upload the file you just downloaded.
--- akismet.php
+++ akismet.php
@@ -1,53 +1,53 @@
 <?php
 /**
- * @version		1.0.0
- * @package		Akismet for Kunena
- * @author		JoomlaWorks http://www.joomlaworks.net
- * @copyright	Copyright (c) 2006 - 2014 JoomlaWorks Ltd. All rights reserved.
- * @license		GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ * @version     1.1.0
+ * @package     Akismet for Kunena
+ * @author      JoomlaWorks https://www.joomlaworks.net
+ * @copyright   Copyright (c) 2006 - 2019 JoomlaWorks Ltd. All rights reserved.
+ * @license     GNU/GPL license: https://www.gnu.org/licenses/gpl.html
  */
 
 // No direct access
-defined('_JEXEC') or die ;
+defined('_JEXEC') or die;
 
 class plgKunenaAkismet extends JPlugin
 {
+    public function onKunenaBeforeSave($context, $table, $isNew)
+    {
+        // Utilize a single language file
+        $this->loadLanguage('plg_kunena_akismet.sys');
 
-	public function onKunenaBeforeSave($context, $table, $isNew)
-	{
-		$this->loadLanguage('plg_kunena_akismet.sys');
-		if ($context == 'com_kunena.KunenaForumMessage' && $this->params->get('apiKey'))
-		{
-			$user = JFactory::getUser();
-			if ($user->authorise('core.login.admin'))
-			{
-				return true;
-			}
-			require_once JPATH_SITE.'/plugins/kunena/akismet/Akismet.class.php';
-			$user = JFactory::getUser();
-			$akismet = new Akismet(JURI::root(false), $this->params->get('apiKey'));
-			$akismet->setCommentAuthor($user->name);
-			$akismet->setCommentAuthorEmail($user->email);
-			$akismet->setCommentContent($table->message);
-			try
-			{
-				$result = $akismet->isCommentSpam();
-				if ($result)
-				{
-					$table->setError(JText::_('PLG_KUNENA_AKISMET_SPAM_DETECTED'));
-					return false;
-				}
-				else
-				{
-					return true;
-				}
-			}
-			catch(Exception $exception)
-			{
-				$table->setError($exception->getMessage());
-				return false;
-			}
-		}
-	}
+        if ($context == 'com_kunena.KunenaForumMessage' && $this->params->get('apiKey')) {
+            $user = JFactory::getUser();
 
+            // Disable for admins
+            if ($user->authorise('core.login.admin')) {
+                return true;
+            }
+
+            // Load the Akismet for PHP class
+            require_once JPATH_SITE.'/plugins/kunena/akismet/Akismet.class.php';
+
+            $akismet = new Akismet(JURI::root(false), $this->params->get('apiKey'));
+            $akismet->setCommentAuthor($user->name);
+            $akismet->setCommentAuthorEmail($user->email);
+            $akismet->setCommentContent($table->message);
+
+            try {
+                $result = $akismet->isCommentSpam();
+
+                if ($result) {
+                    $table->setError(JText::_('PLG_KUNENA_AKISMET_SPAM_DETECTED'));
+
+                    return false;
+                } else {
+                    return true;
+                }
+            } catch (Exception $exception) {
+                $table->setError($exception->getMessage());
+
+                return false;
+            }
+        }
+    }
 }
